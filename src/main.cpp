30514cadfc98050b1245453237049e593b67078e
EnzoGuillouche <enzo.guillouche@algosup.com>
2024-12-15 14:02:41 +0100

    Initiated the project
    The .gitignore has been updated.
    The main.cpp file has been created and is fully linked with the SFML library.
    The images are linked.

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::Texture texture;
+    texture.loadFromFile("img/marioIcon.png");
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+    return 0;
+}
